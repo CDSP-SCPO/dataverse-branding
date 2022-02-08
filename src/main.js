5d9f4c2fb5cc878e96b3f4b6ebb0c6b8009ee6ce
bro <baptiste.rouxel@sciencespo.fr>
2022-02-08 16:56:38 +0100

    Homepage simplified

--- src/main.js
+++ src/main.js
@@ -13,21 +13,9 @@ if (process.env.NODE_ENV != 'development') {
   document.addEventListener("DOMContentLoaded", function() { 
     // remove extra DV attributes
     let html = document.querySelector('html'); html.removeAttribute('xmlns'); html.removeAttribute('xml:lang');
-    let head = document.querySelector('head'); head.removeAttribute('id');
 
-    // remove everything except our content
-    document.querySelectorAll('body > :not(#content)').forEach(e => e.parentNode.removeChild(e));
-
-    // remove every head elements
-    document.querySelectorAll('head > *').forEach(e => e.parentNode.removeChild(e));
-
-    // move elements in the head
-    document.querySelectorAll('#content > :not(div):not(script)').forEach(e => head.appendChild(e));
-
-    // move content in the body (outside of the #content div)
-    let body = document.querySelector('body');
-    document.querySelectorAll('#content > *').forEach(e => body.appendChild(e));
-    let content = document.querySelector('#content'); if (content) content.remove();
+    // replace html content by our own
+    html.innerHTML = document.querySelector('#content').innerHTML;
 
     setup();
   });
